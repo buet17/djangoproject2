84b24b0b7b79592e2a4c22a7596394192c5ad677
Md Noor Alam <nour.buet@gmail.com>
2024-02-17 12:32:28 +0600

    List Dict Values and TAgs in views then call html

--- app1/views.py
+++ app1/views.py
@@ -1,42 +1,50 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
 from app1 import views
 
 # Create your views here.
 
 def index(request):
-    # name="AIB PLC"
+    # template_name=loader.get_template('index.html')
+    # return HttpResponse(template_name.render())
+
     listStd=[
         {
-            'Name':'Mr. Shakhawat',
+            'name':'Mr. Shakhawat',
             'age':22,
-            'ID':170220240011
+            'id':170220240011
         },
         {
-            'Name':'Mr. Rahim',
+            'name':'Mr. Rahim',
             'age':24,
-            'ID':170220240012
+            'id':170220240012
         },
         {
-            'Name':'Mr. Karim',
+            'name':'Mr. Karim',
             'age':21,
-            'ID':170220240013
+            'id':170220240013
         },
     ]
-    total_age=0
-    std_no=len(listStd)
+    # total_age=0
+    # std_no=len(listStd)
 
-    for x in listStd:
-        total_age=total_age+x['age']
-    avg_age1=total_age/std_no
-    print(avg_age1)
-    return render(request, 'index.html',{'Student':'age'})
-    # return HttpResponse(f"Hello World: {name}")
+    # for x in listStd:
+    #     total_age=total_age+x['age']
+    # avg_age1=total_age/std_no
+    # print(avg_age1)
+    return render(request, 'index.html',{'students':listStd})
+    # return render(request, 'index.html',{'AVG':avg_age1})
+
 
 def about(request):
-    return render(request, 'about.html')
+    template_name=loader.get_template('about.html')
+    return HttpResponse(template_name.render())
+    # return render(request, 'about.html')
 
 def contact(request):
-    return render(request, 'contact.html')
+    name="Md Noor Alam"
+    return render(request,'contact.html',{'name':name})
+    # return render(request, 'contact.html')
 
 def services(request):
     return render(request, 'services.html')
